a22ae8571edc7ed15e2130866f27a7ba2ff9919f
Maximiliano Barroso <jmaxbarroso@gmail.com>
2023-02-25 21:08:31 +0100

    Primer commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Damos la bienvenida
+frase_inicial = "Bienvenido"
+# Pedimos al usuario nombre, edad y pais
+nombre = input("Por favor, introduce tu nombre\n")
+edad = input("Por favor, introduce tu edad\n")
+pais = input("Introduce tu país de nacimiento\n")
+# Imprimimos los resultados concatenados
+print(f"{frase_inicial}\nGracias por identificarte {nombre}\nDejamos registrado que tienes {edad} y tu pais de nacimiento es {pais}")
