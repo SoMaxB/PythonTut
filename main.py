559699c8a184a4ee843732d2fcf80a0d14e4007a
Maximiliano Barroso <jmaxbarroso@gmail.com>
2023-02-27 21:29:16 +0100

    Segundo commit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# Damos la bienvenida
-frase_inicial = "Bienvenido"
-# Pedimos al usuario nombre, edad y pais
-nombre = input("Por favor, introduce tu nombre\n")
-edad = input("Por favor, introduce tu edad\n")
-pais = input("Introduce tu país de nacimiento\n")
-# Imprimimos los resultados concatenados
-print(f"{frase_inicial}\nGracias por identificarte {nombre}\nDejamos registrado que tienes {edad} y tu pais de nacimiento es {pais}")
+print("## Calculadora de exponentes ##")
+n1 = input("Elige tu primer numero: ")
+n2 = input("Ahora un segundo numero: ")
+
+expo = int(n1) ** int(n2)
+
+print(f"El resultado de {n1} elevado a {n2} es {expo}.")
