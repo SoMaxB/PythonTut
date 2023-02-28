388158506027c636989236beedeb440af2d75811
Maximiliano Barroso <jmaxbarroso@gmail.com>
2023-02-28 21:18:49 +0100

    Otro

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-print("## Calculadora de exponentes ##")
-n1 = input("Elige tu primer numero: ")
-n2 = input("Ahora un segundo numero: ")
+print("##############################")
+print("###- Calculadora mejorada -###")
+print("##############################")
 
-expo = int(n1) ** int(n2)
+operacion = input("Hola, elija una opción:\n1 - Suma\n2 - Resta\n3 - Multiplicación\n4 - División\n5 - Módulo\n6 - Exponente\nTeclee un número y pulse ENTER\n")
 
-print(f"El resultado de {n1} elevado a {n2} es {expo}.")
+
+print(f"Ha elegido la opción {operacion}")
+
+primero = int(input("Especifique el primer operador:\n"))
+segundo = int(input("Especifique el segundo operador:\n"))
+
+match operacion:
+    case "1":
+        print("Ha seleccionado suma")
+        print(f"La suma es {primero + segundo}")
+    case "2":
+        print("Ha seleccionado resta")
+        print(f"La resta es {primero - segundo}")
+    case "3":
+        print(f"La multiplicacion es {primero * segundo}")
+    case "4":
+        print(f"La división es {primero / segundo}")
+    case "5":
+        print(f"El módulo es {primero % segundo}")
+    case "6":
+        print(f"El exponente es {primero ** segundo}")
+    case _:
+        print("Opción incorrecta")
