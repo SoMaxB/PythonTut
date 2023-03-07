73d38abe5e003c9a81daccbc99d91aff7b2d1c7e
Maximiliano Barroso <jmaxbarroso@gmail.com>
2023-03-07 20:12:28 +0100

    Otro más

--- main.py
+++ main.py
@@ -1,46 +1,11 @@
-print("-> Pizzería PF <-")
+colores = {
+    1 : "rojo",
+    2 : "azul",
+    3 : "verde",
+    4 : "amarillo"
+}
 
-precio = 0
+colores[5] = "blanco"
 
-dinero = int(input("Cuanto dinero tiene para comprar pizzas?:\n"))
-
-margarita = 9
-salami = 11
-bacon = 13
-
-elecciones = input("Elige una pizza:\n1) Margarita\n2) Salami\n3) Bácon\n")
-
-for eleccion in elecciones:
-    if eleccion == 1:
-        print(f"Tu pizza cuesta: {margarita}")
-    elif eleccion == 2:
-        print(f"Tu pizza cuesta: {salami}")
-    elif eleccion == 3:
-        print(f"Tu pizza cuesta: {bacon}")
-print(eleccion)
-
-respuesta = True
-
-input("Desea añadir algún ingrediente:\n")
-if respuesta == "No":
-    #respuesta = False
-    print(f"El valor de su pedido es: {precio}")
-elif respuesta == "Si":
-    input("Puede elegir entre:\n-Huevo\n-Atún\n-Jamón")
-    if respuesta == "Huevo":
-        precio += 1
-    elif respuesta == "Atún":
-        precio += 2
-    elif respuesta == "Jamón":
-        precio += 1
-
-ingredientes = ["Huevo", "Atún", "Jamón"]
-
-'''for ingrediente in ingredientes:
-    ingredientes += str(1)
-
-precio_final = eleccion + ingrediente'''
-
-cambio = dinero - precio
-
-print(f"El valor de su pedido es: {precio}€, usted ha pagado con {dinero}€, su cambio es: {cambio}€")
+for color in colores:
+    print(f"{color} - {colores[color].capitalize()}")
